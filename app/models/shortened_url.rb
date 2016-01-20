5c31bf4e48f256ec9f4dc88f00ebd7e21bee0247
Michael Park <michaelowenpark@gmail.com>
2016-01-20 15:26:22 -0800

    initial

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id           :integer          not null, primary key
+#  long_url     :string           not null
+#  short_url    :string
+#  submitter_id :integer          not null
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class ShortenedUrl < ActiveRecord::Base
+  validates :short_url, uniqueness: true
+  validates :submitter_id, presence: true
+
+  belongs_to :submitter,
+  class_name: :User,
+  foreign_key: :submitter_id,
+  primary_key: :id
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(long_url: long_url,
+                        submitter_id: user.id,
+                        short_url: self.random_code)
+
+  end
+
+  def self.random_code
+    potential_code = SecureRandom.urlsafe_base64
+
+    while ShortenedUrl.exists?(short_url: potential_code)
+      potential_code = SecureRandom.urlsafe_base64
+    end
+
+    potential_code
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  validates :email,
+    presence: true,
+    uniqueness: true
+
+  has_many :shortened_urls,
+  class_name: :ShortenedUrl,
+  foreign_key: :submitter_id,
+  primary_key: :id
+  
+end
